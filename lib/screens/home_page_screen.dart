650e23b511f7891424d6c47cc665b6c188c66b66
renadkfakhoury <renadkazem2002@gmail.com>
2023-11-19 21:34:36 +0200

    3commit

--- lib/screens/home_page_screen.dart
+++ lib/screens/home_page_screen.dart
@@ -5,6 +5,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('images/photo-main.jpg'),
+          ],
+        ),
+    );
   }
 }
